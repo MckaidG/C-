aaf2afda6e8dd1da1a85dc832330b493161b7e19
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-09-30 17:22:15 -0600

    FizzBuzz

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+static class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        for (int i = 1; i <= 100; i++)
+        {
+
+            bool canBeMultipliedByFive = i.CanBeMultipliedBy(5);
+            bool canBeMultipliedByThree = i.CanBeMultipliedBy(3);
+
+            if (canBeMultipliedByFive && canBeMultipliedByThree)
+            {
+
+                Console.WriteLine("{0}: FizzBuzz", i);
+            }
+            else if (canBeMultipliedByThree)
+            {
+                
+                Console.WriteLine("{0}: Fizz", i);
+
+            }
+            else if (canBeMultipliedByFive)
+            {
+
+                Console.WriteLine("{0}: Buzz", i);
+
+            }
+            else
+            {
+
+                Console.WriteLine(i);
+            }
+        }
+
+        Console.ReadLine();
+    }
+
+    private static bool CanBeMultipliedBy(this int sourceNumber, int targetNumber)
+    {
+
+        return (sourceNumber % targetNumber) == 0;
+    }
+}
